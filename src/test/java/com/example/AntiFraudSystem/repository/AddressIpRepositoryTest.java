b6afad9d1861a0b12c7fd36fc7678ba2ca57bfb5
Kamil Madej <kamilmadej1219@gmail.com>
2023-10-02 20:50:57 +0200

    create testing catalog strcuture

---
+++ src/test/java/com/example/AntiFraudSystem/repository/AddressIpRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.example.AntiFraudSystem.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class AddressIpRepositoryTest {
+
+
+    @BeforeEach
+    public void init() {
+
+    }
+}
